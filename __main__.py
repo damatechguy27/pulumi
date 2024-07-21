469812b9ee5de8a7a4eadbd430fc8786ecb92a6c
D M <dmit27@itpro.local>
2024-07-21 19:50:22 -0400

    deploying ecr and image6

--- __main__.py
+++ __main__.py
@@ -2,7 +2,7 @@ import pulumi
 import pulumi_aws as aws
 
 # Create an ECR repository
-repo = aws.ecr.Repository('PulumiECRRepo')
+repo = aws.ecr.Repository('pulumi_ecr_repo')
 
 # Export the repository URL
 pulumi.export('repository_url', repo.repository_url)
